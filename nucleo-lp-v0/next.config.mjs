1466f1174e21570f8b95a2dd8e2b41a5747fe749
ryanezdoren-gif <r.yanez.doren@gmail.com>
2026-01-30 16:19:17 -0300

    Update next.config.mjs

--- nucleo-lp-v0/next.config.mjs
+++ nucleo-lp-v0/next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  experimental: { serverActions: { allowedOrigins: ["*"] } }
+reactStrictMode: true,
+experimental: { serverActions: { allowedOrigins: ["*"] } },
+typescript: { ignoreBuildErrors: true },
+eslint: { ignoreDuringBuilds: true }
 };
 export default nextConfig;

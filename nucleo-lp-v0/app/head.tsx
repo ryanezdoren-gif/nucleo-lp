61d9368703810bdfb47d524f0f6741a2f46a358f
ryanezdoren-gif <r.yanez.doren@gmail.com>
2026-01-30 18:19:10 -0300

    Create head.tsx

---
+++ nucleo-lp-v0/app/head.tsx
@@ -0,0 +1,12 @@
+export default function Head() {
+return (
+<>
+<meta charSet="utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+<link
+rel="stylesheet"
+href="https://cdn.jsdelivr.net/npm/tailwindcss@3.4.9/dist/tailwind.min.css"
+/>
+</>
+);
+}
